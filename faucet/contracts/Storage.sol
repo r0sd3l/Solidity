795f8a5be2ea406961ff50d7bb5e39b3ff6b999c
r0sd3l <cryptomano91@protonmail.com>
2022-02-08 21:34:09 -0600

    added storage contract - learning memory

--- faucet/contracts/Storage.sol
+++ faucet/contracts/Storage.sol
@@ -7,9 +7,10 @@ contract Storage {
     address public c = 0x5a139E63B10e867CF51deB4e7433838dC3158A0f;
     bool d = true;
     uint64 public e = 15;
-
-    uint256 public f = 200;
-    uint8 public g = 40;
-    uint256 public h = 789;
+    // 32 bytes, all values will be stored in slot 0
+    // 0x 0f 01 5a139e63b10e867cf51deb4e7433838dc3158a0f 000a 07
+    uint256 public f = 200; // 32 bytes -> slot 1
+    uint8 public g = 40; // 1byte - slot 2
+    uint256 public h = 789; // 32 bytes - slot 3
 
 }
